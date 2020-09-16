2f9b4ffc81e434253a3ad6160af449d78c5dd112
Marcin Pajkowski <marcin.pajkowski@gmail.com>
2020-09-16 22:11:42 +0200

    śro, 16 wrz 2020, 22:59:12 CEST

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+;; packaging
 (require 'package)
 
 (setq package-archives
@@ -7,11 +8,18 @@
 
 (package-initialize)
 
-(setq make-backup-files nil)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-(setq indent-tabs-mode nil)
-(define-key text-mode-map (kbd "<tab>") 'tab-to-tab-stop)
-(setq tab-stop-list (number-sequence 4 120 4))
+(require 'use-package)
+
+(setq make-backup-files nil)
+(setq inhibit-startup-message t)
+(setq initial-scratch-message nil)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(setq-default yes-or-no-p 'y-or-n-p)
 
 (use-package super-save
   :ensure t
@@ -23,15 +31,6 @@
   (setq dashboard-items '((recents . 5)
 			  (projects . 10)))
   (dashboard-setup-startup-hook))
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(setq inhibit-startup-message t)
-(setq initial-scratch-message nil)
-
-(require 'use-package)
 
 (electric-pair-mode 1)
 
@@ -50,7 +49,30 @@
   (evil-define-key 'normal 'global (kbd "SPC j") 'evil-window-down)
   (evil-define-key 'normal 'global (kbd "SPC k") 'evil-window-up)
   (evil-define-key 'normal 'global (kbd "SPC l") 'evil-window-right)
+  (evil-define-key 'normal 'global (kbd "SPC <left>") 'evil-window-left)
+  (evil-define-key 'normal 'global (kbd "SPC <down>") 'evil-window-down)
+  (evil-define-key 'normal 'global (kbd "SPC <up>") 'evil-window-up)
+  (evil-define-key 'normal 'global (kbd "SPC <right>") 'evil-window-right)
   (evil-mode))
+
+(use-package counsel
+  :config
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t)
+  (global-set-key "\C-s" 'swiper)
+  (global-set-key (kbd "C-c C-r") 'ivy-resume)
+  (global-set-key (kbd "<f6>") 'ivy-resume)
+  (global-set-key (kbd "M-x") 'counsel-M-x)
+  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+  (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+  (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+  (global-set-key (kbd "<f1> o") 'counsel-describe-symbol)
+  (global-set-key (kbd "<f1> l") 'counsel-find-library)
+  (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+  (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+  (global-set-key (kbd "C-x l") 'counsel-locate)
+  (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+  (ivy-mode 1))
 
 (use-package magit
   :config
@@ -66,6 +88,7 @@
   (setq read-process-output-max (* 1024 1024))
   (setq gc-cons-threshold 10000000))
 
+(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
 (use-package lsp-ui :commands lsp-ui-mode)
 
 
@@ -74,41 +97,15 @@
 (setq uniquify-separator "/"               ;; The separator in buffer names.
       uniquify-buffer-name-style 'forward) ;; names/in/this/style
 
-(use-package helm
- :diminish
- :init (helm-mode t)
- :bind (("M-x"     . helm-M-x)
-        ("C-x C-f" . helm-find-files)
-        ("C-x b"   . helm-mini)     ;; See buffers & recent files; more useful.
-        ("C-x r b" . helm-filtered-bookmarks)
-        ("C-x C-r" . helm-recentf)  ;; Search for recently edited files
-        ("C-c i"   . helm-imenu)
-        ("C-h a"   . helm-apropos)
-        ;; Look at what was cut recently & paste it in.
-        ;;("M-y" . helm-show-kill-ring)
-
-        :map helm-map
-        ;; We can list ‘actions’ on the currently selected item by C-z.
-        ("C-z" . helm-select-action)
-        ;; Let's keep tab-completetion anyhow.
-        ("TAB"   . helm-execute-persistent-action)
-        ("<tab>" . helm-execute-persistent-action)))
-
-(use-package helm-projectile
-  :config (helm-projectile-on))
-
 (use-package rg
   :config
   (global-set-key (kbd "M-s g") 'rg)
   (global-set-key (kbd "M-s d") 'rg-dwim))
 
-(use-package helm-rg)
-
 (use-package flycheck)
 
 (use-package rustic
   :config
-  (setq lsp-rust-analyzer-server-command "/home/mpa/.local/bin/rust-analyzer")
   (setq rustic-lsp-server 'rust-analyzer))
 
 (use-package company
@@ -126,6 +123,10 @@
   :config
  (define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map)
  (projectile-mode +1))
+
+(use-package all-the-icons
+  :config
+  (setq all-the-icons-color-icons t))
 
 (use-package treemacs
   :defer t
@@ -206,6 +207,10 @@
 (use-package treemacs-magit
   :after treemacs magit)
 
+(use-package treemacs-all-the-icons
+  :config
+  (treemacs-load-theme "all-the-icons"))
+
 (use-package which-key
     :config
     (which-key-mode))
@@ -228,6 +233,11 @@
 (evil-define-key 'normal 'global (kbd "SPC b d") (lambda() (interactive) (kill-buffer (current-buffer))))
 (evil-define-key 'normal 'global (kbd "z s") (lambda() (interactive) (save-buffer (current-buffer))))
 
+;; tabs to spaces
+(setq indent-tabs-mode nil)
+(define-key text-mode-map (kbd "<tab>") 'tab-to-tab-stop)
+(setq tab-stop-list (number-sequence 4 120 4))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -240,4 +250,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(zzz-to-char real-auto-save dashboard yasnippet which-key use-package treemacs-projectile treemacs-magit treemacs-icons-dired treemacs-evil rustic rg lsp-mode hybrid-reverse-theme helm-rg flycheck company)))
+   '(all-the-icons-ivy-rich all-the-icons-ivy treemacs-all-the-icons all-the-icons dired dire lsp-ivy counsel lsp-ui zzz-to-char real-auto-save dashboard yasnippet which-key use-package treemacs-projectile treemacs-magit treemacs-icons-dired treemacs-evil rustic rg lsp-mode hybrid-reverse-theme flycheck company)))
