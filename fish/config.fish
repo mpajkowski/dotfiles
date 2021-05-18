210e64fb2688cecb59a736c385ab5321b351c4a8
Marcin Pajkowski <marcin.pajkowski@gmail.com>
2021-05-18 02:08:32 +0200

    Tue May 18 02:08:35 CEST 2021

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,5 @@ set -U fish_user_paths $fish_user_paths "$HOME/.local/bin"
 set -U fish_user_paths $fish_user_paths "$HOME/.cargo/bin"
 set -U fish_user_paths $fish_user_paths "$HOME/diff-so-fancy"
 set -U fish_user_paths $fish_user_paths "$HOME/.fzf/bin"
+set -U fish_user_paths $fish_user_paths "/snap/bin"
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
